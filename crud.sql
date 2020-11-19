26a4358b90ef901326c285cb6a2a5acc4cb44213
woo90 <wnsdl980@gmail.com>
2020-11-19 11:35:06 +0900

    회원 가입 수정중

--- crud.sql
+++ crud.sql
@@ -1,9 +1,6 @@
--- INSERT INTO books SET title = '구운몽전', writer = '운몽이', content = '꿈을 꾸었네...';
--- UPDATE books SET title='별주부전', writer='거북이', content='죽다 살았네...' WHERE id=3;
--- DELETE FROM books WHERE id=4;
--- SELECT title, writer FROM books;
--- SELECT * FROM books;
--- SELECT * FROM books ORDER BY id ASC;
--- SELECT * FROM books ORDER BY id DESC;
--- SELECT * FROM books WHERE id=3;
- SELECT * FROM books ORDER BY id DESC LIMIT 1, 3;
+
+
+-- SELECT COUNT(id) FROM books
+-- INSERT INTO users SET userid='woo', userpw='000000
+-- SELECT * FROM users WHERE userid='woo';
+SELECT * FROM users WHERE userid LIKE '%woo%';
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const error = require('http-errors');
+const bcrypt = require('bcrypt');
 const { pool, sqlGen } = require('../modules/mysql-conn');
+const { alert } = require('../modules/util');
 
 router.get('/join', (req, res, next) => {
 	const pug = { 
@@ -10,6 +13,44 @@ router.get('/join', (req, res, next) => {
 	}
 	res.render('user/join', pug);
 })
+
+router.post('/save', async (req, res, next) => {
+	try {
+		req.body.userpw = await bcrypt.hash(req.body.userpw + process.env.BCRYPT_SALT, Number(process.env.BCRYPT_ROUND));
+		let rs = await sqlGen('users', 'I', {
+			field: ['userid', 'userpw', 'username', 'email'],
+			data: req.body
+		})
+		if(rs[0].affectedRows == 1)
+			res.send(alert('회원가입이 완료되었습니다. 로그인 해 주세요.', '/user/login'));
+		else
+			res.send(alert('회원가입이 완료되었습니다. 다시 시도해 주세요.', '/user/join'));
+	}
+	catch(e) {
+		next(error(500, e.sqlMessage || e));
+	}
+});
+
+router.get('/login', (req, res, next) => {
+	const pug = { 
+		file: 'user-login', 
+		title: '회원 로그인',
+		titleSub: '회원 로그인 후 서비스를 이용하세요!'
+	}
+	res.render('user/login', pug);
+})
+
+router.post('/logon', async (req, res, next) => {
+	try {
+		let rs = await sqlGen('users', 'S', {
+			field: ['userid'], where:['userid', req.body.userid]
+		});
+		res.json(re[0]);
+	}
+	catch(e) {
+		next(error(500, e.sqlMessage || e));
+	}
+});
 
 
 router.get('/idchk/:userid', async (req, res, next) => {
